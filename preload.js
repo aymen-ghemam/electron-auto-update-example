0941876cf2b295848966a1291d6ff2a4687e515b
aymen-ghemam <gh.aymen39@gmail.com>
2022-08-09 21:32:39 +0100

    auto-updating app example

---
+++ preload.js
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    ping: () => ipcRenderer.invoke('ping'),
+    restart: () => ipcRenderer.invoke('restart_app'),
+    appVersion : () => ipcRenderer.invoke('app_version'),
+    onUpdateAvailable: (handler) => ipcRenderer.on('update_available', handler),    
+    onUpdateDownloaded: (handler) => ipcRenderer.on('update_downloader', handler),    
+});
