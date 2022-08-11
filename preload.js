16e043bd74d50951df6304a5d725c343150187ad
unknown <gh.aymen39@gmail.com>
2022-08-11 15:50:32 +0100

    fix onUpdateDownloded issue

--- preload.js
+++ preload.js
@@ -5,5 +5,5 @@ contextBridge.exposeInMainWorld('electronAPI', {
     restart: () => ipcRenderer.invoke('restart_app'),
     appVersion : () => ipcRenderer.invoke('app_version'),
     onUpdateAvailable: (handler) => ipcRenderer.on('update_available', handler),    
-    onUpdateDownloaded: (handler) => ipcRenderer.on('update_downloader', handler),    
+    onUpdateDownloaded: (handler) => ipcRenderer.on('update_downloaded', handler),    
 });
